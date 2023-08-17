28bbb7f884dbbb62149c2ca83d623cc9774cb32d
trhgquan <quanthcstpbt@gmail.com>
2023-08-17 20:56:45 +0700

    Don't need too much files for a single ML code

--- code/tfidf-svm/model.py
+++ code/tfidf-svm/model.py
@@ -1,14 +1,14 @@
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline           
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 
-def create_pipeline():
+def create_pipeline(classifier=SVC):
     ngram_range = (1, 2)
     pipeline = Pipeline([
         ("count", CountVectorizer(ngram_range=ngram_range)),
         ("tfidf", TfidfTransformer(use_idf=True)),
-        ("svc", SVC(random_state=42)),
+        ("clf", classifier(random_state=42)),
     ])
 
     return pipeline
--- code/tfidf-svm/train.py
+++ code/tfidf-svm/train.py
@@ -1,5 +1,8 @@
 #!/bin/python3
 
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+
 from argparse import ArgumentParser
 from utils import clean_df
 from model import create_pipeline
@@ -10,6 +13,7 @@ import pickle as pkl
 def main():
     parser = ArgumentParser()
     parser.add_argument("--train", default="train.csv")
+	parser.add_argument("--classifier", default="svc")
     parser.add_argument("--model_path", default="model.pkl")
     parser.add_argument("--tokenizer_path", default="tokenizer.pkl")
 
@@ -19,7 +23,13 @@ def main():
 
     train_df_X, train_df_y = train_df["text"], train_df["target"]
 
-    pipeline = create_pipeline()
+	classifier = "svc"
+	if args.classifier == "rf":
+		classifier = RandomForestClassifier
+	if args.classifier == "svc":
+		classifier = SVC
+
+    pipeline = create_pipeline(classifier = classifier)
 
     pipeline.fit(train_df_X, train_df_y)
 
